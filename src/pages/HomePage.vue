643ecf4a4a39d81aa33e214686eb4f56a8fc9aec
solohad1n <fox663399@gmail.com>
2024-02-12 13:30:12 +0600

    add typography and color root

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Typography from '@/shared/typography/TheTypography.vue'
+</script>
+
+<template>
+  <div>
+    <Typography tagName="p" size="xs">1</Typography>
+    <Typography tagName="p" size="s">2</Typography>
+    <Typography tagName="p" size="m">3</Typography>
+    <Typography tagName="p" size="l">4</Typography>
+    <Typography tagName="p" size="xl">5</Typography>
+    <br/>
+    <Typography tagName="p" size="xs" bold>1</Typography>
+    <Typography tagName="p" size="s" bold>2</Typography>
+    <Typography tagName="p" size="m" bold>3</Typography>
+    <Typography tagName="p" size="l" bold>4</Typography>
+    <Typography tagName="p" size="xl" bold>5</Typography>
+    <br/>
+    <Typography tagName="h5">1</Typography>
+    <Typography tagName="h4">2</Typography>
+    <Typography tagName="h3">3</Typography>
+    <Typography tagName="h2">4</Typography>
+    <Typography tagName="h1">5</Typography>
+  </div>
+</template>
+
+
+<style>
+
+</style>
