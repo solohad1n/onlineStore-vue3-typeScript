f775c3811bf7fa3e121b344721badfd037819d19
solohad1n <fox663399@gmail.com>
2024-02-19 17:53:00 +0600

    adaptive layout Footer

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import { Header } from '@/widgets/Header'
 import { BottomTabNavigator } from '@/widgets/bottom-tab-navigator'
+import { Footer } from '@/widgets/footer'
+import { Content } from '@/shared/content'
 </script>
 
 <template>
-  <div>
     <Header/>
-  </div>
-  <div>
     <BottomTabNavigator/>
-  </div>
+    <Content/>
+    <Footer/>
 </template>
 
 
